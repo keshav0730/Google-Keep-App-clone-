447c0c5ef3ae90b08f98e61c311c351d00de0a07
keshav <kumarmishra0301@gmail.com>
2021-08-30 04:04:40 +0530

    Create Note Component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import Header from './Header';
 import Footer from './Footer';
+import CreateNote from './CreateNote';
+
 function App() {
   return (
    <>
      <Header/>
+     <CreateNote/>
      <Footer/>
+    
    </>
   );
 }
---
+++ src/CreateNote.js
@@ -0,0 +1,11 @@
+import React from "react";
+const CreateNote = () => {
+   
+    return (
+        <>
+           <h1>This is Create Note</h1>
+
+        </>
+    )
+}
+export default CreateNote;
